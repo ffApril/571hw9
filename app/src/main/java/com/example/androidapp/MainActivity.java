f7e4e30d81dded21e3f9f969bf6163a97408faf8
ffApril <747840349@qq.com>
2023-04-29 00:13:08 -0700

    feature:finish clear button

--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.androidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.ArrayAdapter;
+import android.widget.Switch;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,6 +25,41 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setSelection(0);
+
+        Button btn_clear = findViewById(R.id.btn_clear);
+        Button btn_search = findViewById(R.id.btn_search);
+        EditText keyword = findViewById(R.id.keyword);
+        EditText distance = findViewById(R.id.distance);
+        EditText location = findViewById(R.id.location);
+        Switch auto = findViewById(R.id.auto);
+        distance.setText("10");
+        btn_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 在这里执行你想要实现的逻辑
+                // ...
+                keyword.setText("");
+                distance.setText("10");
+                spinner.setSelection(0);
+                location.setText("");
+                auto.setChecked(false);
+
+            }
+        });
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 在这里执行你想要实现的逻辑
+                // ...
+                String text = keyword.getText().toString().trim();
+                if (TextUtils.isEmpty(text)) {
+                    keyword.setError("This field cannot be empty.");
+
+                }
+
+
+            }
+        });
 
     }
 
