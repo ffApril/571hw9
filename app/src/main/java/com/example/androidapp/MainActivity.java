f5e8ebf24eb776acd74189fbcdb308ff353bf858
ffApril <747840349@qq.com>
2023-04-28 18:01:05 -0700

    from

--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -3,7 +3,8 @@ package com.example.androidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.my_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.spinner_items, R.layout.spinner_text);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(0);
+
     }
+
+
 }
